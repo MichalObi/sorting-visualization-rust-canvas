e1950b1e4cef1a33f7bdf9e155a79a1915fb6ee4
MichalObi <ndwz123@gmail.com>
2019-02-11 21:58:24 +0100

    Initial commit - base project structure and compile wasm test fn

---
+++ script.js
@@ -0,0 +1,7 @@
+(function() {
+  const webassembly_js = import('./../pkg/rust-sorting.js');
+  // webassembly_js.then(js => {
+  //   console.log(js.greet("WebAssembly"));
+  //   const canvas = document.getElementById('canvas');
+  // });
+}())
