ec14e6591c3d4aea64f7727cc48d557544b2e5ca
MichalObi <ndwz123@gmail.com>
2019-02-13 17:18:58 +0100

    Pass algo type to rust

--- rust-sorting/src/lib.rs
+++ rust-sorting/src/lib.rs
@@ -2,6 +2,6 @@ extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn get_test_text() -> String {
-    "Hello from rust!".into()
+pub fn rust_sort(algo: String) -> String {
+    algo
 }
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const webassembly_js = import('./rust-sorting/pkg/rust_sorting.js');
 webassembly_js.then(wasmModule => {
-  const webassembkyText = wasmModule.get_test_text();
+  const webassembkyText = wasmModule.rust_sort('bubble');
   const ctx = document.getElementById('canvas').getContext('2d');
   ctx.font = '48px serif';
   ctx.fillText(webassembkyText, 10, 50);
