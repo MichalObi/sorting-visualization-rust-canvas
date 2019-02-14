2d8604de240db6fe83ddb7f2f85fb1ca160e3703
MichalObi <ndwz123@gmail.com>
2019-02-14 21:50:55 +0100

    Send js array to rust

--- rust-sorting/src/lib.rs
+++ rust-sorting/src/lib.rs
@@ -1,6 +1,21 @@
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
+// #[wasm_bindgen]
+// pub fn print_js_error() -> String {
+//     "Some error".into()
+// }
+//
+// #[wasm_bindgen]
+// pub fn sort_js_array(array: Box<[JsValue]>) -> Box<[JsValue]>  {
+//     vec![JsValue::NULL, JsValue::UNDEFINED].into_boxed_slice()
+// }
+
+#[wasm_bindgen]
+pub fn send_js_array(array: Option<Box<[JsValue]>>) -> Option<Box<[JsValue]>> {
+    array
+}
+
 #[wasm_bindgen]
 pub fn rust_sort(algo: String) -> String {
     algo
