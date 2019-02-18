0ff1ad44ab55514079e49d5e095c9a0dd516c475
MichalObi <ndwz123@gmail.com>
2019-02-18 22:19:19 +0100

    Use pattern matching to pass not empty array to js

--- rust-sorting/src/lib.rs
+++ rust-sorting/src/lib.rs
@@ -1,19 +1,23 @@
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
-// #[wasm_bindgen]
-// pub fn print_js_error() -> String {
-//     "Some error".into()
-// }
-//
-// #[wasm_bindgen]
-// pub fn sort_js_array(array: Box<[JsValue]>) -> Box<[JsValue]>  {
-//     vec![JsValue::NULL, JsValue::UNDEFINED].into_boxed_slice()
-// }
+#[wasm_bindgen]
+pub fn sort_js_array(array: Box<[JsValue]>) -> Box<[JsValue]>  {
+    array
+}
 
 #[wasm_bindgen]
-pub fn send_js_array(array: Option<Box<[JsValue]>>) -> Option<Box<[JsValue]>> {
-    array
+pub fn send_js_array(array: Option<Box<[JsValue]>>) -> Box<[JsValue]> {
+    match array {
+        Some (ref a) => {
+            let array_cpy = a.clone();
+            sort_js_array(array_cpy)
+        },
+        None => {
+            let error_msg = "Some error ...";
+            Box::new([JsValue::from_str(&error_msg)])
+        }
+    }
 }
 
 #[wasm_bindgen]
