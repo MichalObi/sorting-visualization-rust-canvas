6253bae0510e7450472a7c13f9633537dadce60a
MichalObi <ndwz123@gmail.com>
2019-02-19 20:09:35 +0100

    Rust fmt on lib.rs

--- rust-sorting/src/lib.rs
+++ rust-sorting/src/lib.rs
@@ -2,17 +2,17 @@ extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn sort_js_array(array: Box<[JsValue]>) -> Box<[JsValue]>  {
+pub fn sort_js_array(array: Box<[JsValue]>) -> Box<[JsValue]> {
     array
 }
 
 #[wasm_bindgen]
 pub fn send_js_array(array: Option<Box<[JsValue]>>) -> Box<[JsValue]> {
     match array {
-        Some (ref a) => {
+        Some(ref a) => {
             let array_cpy = a.clone();
             sort_js_array(array_cpy)
-        },
+        }
         None => {
             let error_msg = "Some error ...";
             Box::new([JsValue::from_str(&error_msg)])
