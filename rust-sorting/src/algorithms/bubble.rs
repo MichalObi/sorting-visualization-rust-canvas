f10a930d0ad956f3bfa036209ba5b9a017ca993e
MichalObi <ndwz123@gmail.com>
2019-03-13 19:27:23 +0100

    Algo test passing / do not fire log fn on test run

--- rust-sorting/src/algorithms/bubble.rs
+++ rust-sorting/src/algorithms/bubble.rs
@@ -7,6 +7,8 @@ use crate::SortArray;
 use js_sys::{Array, Math};
 use wasm_bindgen::prelude::JsValue;
 use wasm_bindgen_test::*;
+
+#[cfg(not(test))]
 use web_sys::{console, window};
 
 pub struct BubbleSort;
@@ -15,33 +17,35 @@ pub trait Algorithm {
     fn sort(array: SortArray) -> JsValue;
 }
 
+#[cfg(not(test))]
 fn log_performance(performance: JsValue) {
     console::log_2(&"Rust bubble sort time info (ms): ".into(), &performance);
 }
 
+#[cfg(not(test))]
 fn measure_performance() -> f64 {
     window().unwrap().performance().unwrap().now()
 }
 
 #[wasm_bindgen_test]
 fn test_algo() {
-    let js_array_to_sort_as_string =
-        Array::of3(&JsValue::from(1), &JsValue::from(3), &JsValue::from(2)).to_string();
-    let array = JsValue::from(&js_array_to_sort_as_string);
+    let js_array_to_sort = Array::of3(&JsValue::from(1), &JsValue::from(3), &JsValue::from(2));
+    let js_array_after_sort = Array::of3(&JsValue::from(3), &JsValue::from(2), &JsValue::from(1));
+    let array = JsValue::from(&js_array_to_sort);
 
-    let js_array_after_sort_as_string =
-        Array::of3(&JsValue::from(1), &JsValue::from(2), &JsValue::from(3)).to_string();
-
+    // to compare need data as string not array
     assert_eq!(
-        JsValue::from(&js_array_after_sort_as_string),
-        BubbleSort::sort(SortArray::new(array))
+        JsValue::from(&js_array_after_sort).as_string(),
+        BubbleSort::sort(SortArray::new(array)).as_string()
     );
 }
 
 impl Algorithm for BubbleSort {
     fn sort(array: SortArray) -> JsValue {
+        #[cfg(not(test))]
         let sort_start: f64 = measure_performance();
         let len: u32 = array.get_array_len();
+
         for i in 0..len - 1 {
             let last: u32 = len - i - 1;
 
@@ -63,6 +67,7 @@ impl Algorithm for BubbleSort {
             }
         }
 
+        #[cfg(not(test))]
         log_performance(JsValue::from_f64(measure_performance() - sort_start));
         array.state.clone()
     }
