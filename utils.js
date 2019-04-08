0f00d09cfbe96ad2ac279bb0ae0a6a2edddd37cf
MichalObi <ndwz123@gmail.com>
2019-04-08 21:06:32 +0200

    Save array states and counter

--- utils.js
+++ utils.js
@@ -2,6 +2,13 @@ export const current_array_state = array => {
   updateCanvasWithNewState(array)
 }
 
+let counter = 0,
+    states = [];
+
 const updateCanvasWithNewState = array => {
-  console.log('array', array);
+  ++counter
+  states.push(JSON.stringify(array));
+  
+  const ctx = document.getElementById('canvas').getContext('2d');
+  ctx.clearRect(0, 0, 500, 500);
 }
