98b50f95955e333228d4358e2c35f1ed384acae5
MichalObi <ndwz123@gmail.com>
2019-04-10 20:24:21 +0200

    Basic sort visualization on canvas for bubble sort

--- utils.js
+++ utils.js
@@ -10,23 +10,24 @@ export const current_array_state = array => {
 
 export const update_canvas_with_new_state = () => {
   for (let i = 0, arrayStatesLength = arrayStates.length; i < arrayStatesLength; i++) {
-    ctx.clearRect(0, 0, 800, 400);
-    ctx.fillStyle = '#000';
-    ctx.fillRect(0, 0, 800, 400);
-
-    if (i === 0) {  // tmp
-      console.log('arrayStates[i])', arrayStates[i]);
-      window.requestAnimationFrame(() => drawState(arrayStates[i]));
-    }
+    (index => {
+     setTimeout(() => {
+       ctx.clearRect(0, 0, 800, 400);
+       ctx.fillStyle = '#000';
+       ctx.fillRect(0, 0, 800, 400);
+       drawState(arrayStates[i]);
+    }, i * 500);
+    })(i);
   }
 }
 
 const drawState = currentState => {
-    for (let i = 1, currentStateLength = currentState.length; i <= currentStateLength; i++) {
-      ctx.lineWidth = 4;
+    for (let i = 0, currentStateLength = currentState.length; i < currentStateLength; i++) {
+      const xPosition = 800 - (75 * (i + 1)), canvasWidth = 400;
+      ctx.lineWidth = 20;
       ctx.beginPath();
-      ctx.moveTo(20 * i, 20);
-      ctx.lineTo(20 * i, currentState[i] === 10 ? 380 : currentState[i] * 380 / 10);
+      ctx.moveTo(xPosition, canvasWidth);
+      ctx.lineTo(xPosition, currentState[i] * canvasWidth / currentStateLength);
       ctx.strokeStyle = "#FFF"
       ctx.stroke();
     }
