f7010b3e0042aa0003af6e236193c532f79c7017
Michal <ndwz123@gmail.com>
2019-04-17 10:42:08 +0200

    Add quick sort visualization

--- rust-sorting/src/algorithms/quick.rs
+++ rust-sorting/src/algorithms/quick.rs
@@ -6,21 +6,27 @@ pub struct QuickSort;
 
 impl Algorithm for QuickSort {
     fn sort(array: SortArray, with_visual: bool, speed: &JsValue) -> JsValue {
-        sort_slice(&array, 0, array.get_array_len() - 1)
+        sort_slice(&array, 0, array.get_array_len() - 1, with_visual, speed)
     }
 }
 
-fn sort_slice(array: &SortArray, low: u32, high: u32) -> JsValue {
+fn sort_slice(array: &SortArray, low: u32, high: u32, with_visual: bool, speed: &JsValue) -> JsValue {
     if low < high {
-        let pivot = partition(array, low, high);
-        sort_slice(array, low, pivot - 1);
-        sort_slice(array, pivot + 1, high)
+        let pivot = partition(array, low, high, with_visual);
+        sort_slice(array, low, pivot - 1, with_visual, speed);
+        sort_slice(array, pivot + 1, high, with_visual, speed)
     } else {
+        if with_visual {
+            #[cfg(not(test))]
+            use crate::update_canvas_with_new_state;
+            update_canvas_with_new_state(speed)
+        }
+
         array.state.clone()
     }
 }
 
-fn partition(array: &SortArray, low: u32, high: u32) -> u32 {
+fn partition(array: &SortArray, low: u32, high: u32, with_visual: bool) -> u32 {
     let pivot_js_val = array.get_array_val_by_index(&JsValue::from(high));
     let pivot_f64_val: f64 = pivot_js_val.as_f64().unwrap();
 
@@ -32,9 +38,23 @@ fn partition(array: &SortArray, low: u32, high: u32) -> u32 {
 
         if current_f64_val <= pivot_f64_val {
             array.swap(&JsValue::from(i), &JsValue::from(j));
+
+            if with_visual {
+                #[cfg(not(test))]
+                use crate::current_array_state;
+                current_array_state(&array.state.clone())
+            }
+
             i += 1;
         }
     }
     array.swap(&JsValue::from(i), &JsValue::from(high));
+
+    if with_visual {
+        #[cfg(not(test))]
+        use crate::current_array_state;
+        current_array_state(&array.state.clone())
+    }
+
     i
 }
