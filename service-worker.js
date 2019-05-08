4cc2f12d1c8296e2b3b07ba166718368794063d7
Michal <ndwz123@gmail.com>
2019-05-08 20:29:41 +0200

    Quickfix - hardcode file names of webpack generator

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,23 @@
 const CACHE_NAME = 'sorting-rust-canvas';
 
 let filesToCache = [
+  '/',
   '/index.html',
   '/script.js',
   '/utils.js',
   '/rust-sorting/pkg/rust_sorting.js',
-  '/rust-sorting/pkg/rust_sorting_bg.wasm'
+  '/rust-sorting/pkg/rust_sorting_bg.wasm',
+  // tmp tests
+  '/1.script.js',
+  '/0.script.js',
+  '/d31927af4ebb7d9fc1cd.module.wasm',
+  '/manifest.json',
+  '/images/icons/icon-72x72.png',
+  '/images/icons/icon-96x96.png',
+  '/images/icons/icon-128x128.png',
+  '/images/icons/icon-144x144.png',
+  '/images/icons/icon-152x152.png',
+  '/images/icons/icon-192x192.png'
 ];
 
 self.addEventListener('install', evt => {
