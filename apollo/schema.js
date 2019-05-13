8954631a78106f35ed1b734a03c588cf280e3c2c
Michal <ndwz123@gmail.com>
2019-05-13 21:21:05 +0200

    Add base type and enum for algo confg

--- apollo/schema.js
+++ apollo/schema.js
@@ -7,6 +7,20 @@ const typeDefs = gql `
     "A simple type for getting started!"
     hello: String
   }
+
+  enum ALGO_TYPE {
+    BUBBLE,
+    QUICK,
+    MERGE
+  }
+
+  type AppConfig {
+    id: ID!,
+    algoType: ALGO_TYPE,
+    withVisual: Boolean,
+    speed: Int,
+    array: String
+  }
 `;
 
 exports.typeDefs = typeDefs;
